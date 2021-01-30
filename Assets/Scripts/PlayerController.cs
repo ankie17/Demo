a0a0a8cc0814521011de37f134a110eb17726008
Lucas-Sholtz <lucas.sholtz@gmail.com>
2021-01-30 19:23:15 +0200

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum Direction
+{
+    Stay=0,
+    Right,
+    Left,
+    Top,
+    Down
+}
+public class PlayerController : MonoBehaviour
+{
+
+    public float MoveSpeed = 5.0f;
+    private Direction moveDirection = Direction.Stay;
+    private Transform transform;
+    public float horizontalInput;
+    public float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform = GetComponent<Transform>(); 
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //получить инпут стрелки с клавиатуры
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        if (horizontalInput != 0)
+        {
+            verticalInput = 0;
+        }
+        //изменить направление движения
+        if (horizontalInput == -1)
+        {
+            moveDirection = Direction.Left;
+        }
+        if(horizontalInput == 1)
+        {
+            moveDirection = Direction.Right;
+        }
+        if (verticalInput == -1)
+        {
+            moveDirection = Direction.Down;
+        }
+        if (verticalInput == 1)
+        {
+            moveDirection = Direction.Top;
+        }
+        //изменить координату объект, передвинув его на несколько условных единиц
+        float moveDelta = MoveSpeed * Time.fixedDeltaTime;
+        if (moveDirection == Direction.Left)
+        {
+            transform.position = new Vector3(transform.position.x - moveDelta, transform.position.y, 0);
+        }
+        if(moveDirection == Direction.Right)
+        {
+            transform.position = new Vector3(transform.position.x + moveDelta, transform.position.y, 0);
+        }
+        if (moveDirection == Direction.Top)
+        {
+            transform.position = new Vector3(transform.position.x,transform.position.y + moveDelta, 0);
+        }
+        if(moveDirection == Direction.Down)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - moveDelta, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/StarterCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarterCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        Debug.Log("Awake on Starter Camera");
+    }
+    private void Start()
+    {
+        Debug.Log("Start on Starter Camera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update on Starter Camera");
+    }
+
+    private void LateUpdate()
+    {
+        Debug.Log("LateUpdate on Starter Camera");
+    }
+}
